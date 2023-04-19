bda1445807c762cf762ea0b6e6bdd349c377caee
Evan Blaudy <evan@cg-wire.com>
2023-04-19 04:06:57 +0200

    [qa] black

--- setup.py
+++ setup.py
@@ -3,30 +3,28 @@
 from distutils.core import setup
 import edl
 
-requires = [
-    'timecode'
-]
+requires = ["timecode"]
 
 setup(
-    name='edl',
+    name="edl",
     version=edl.__version__,
-    description='Simple EDL reading library',
-    author='Simon Hargreaves',
-    author_email='simon@simon-hargreaves.com',
-    url='http://www.simon-hargreaves.com/python-edl',
-    packages=['edl'],
+    description="Simple EDL reading library",
+    author="Simon Hargreaves",
+    author_email="simon@simon-hargreaves.com",
+    url="http://www.simon-hargreaves.com/python-edl",
+    packages=["edl"],
     classifiers=[
-       'Development Status :: 3 - Alpha',
-       'Environment :: Console',
-       'Intended Audience :: Developers',
-       'License :: OSI Approved :: MIT License',
-       'Operating System :: MacOS :: MacOS X',
-       'Operating System :: Microsoft :: Windows',
-       'Operating System :: POSIX',
-       'Programming Language :: Python',
-       'Natural Language :: English',
-       'Topic :: Multimedia :: Video',
-       'Topic :: Software Development :: Libraries'
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "Topic :: Multimedia :: Video",
+        "Topic :: Software Development :: Libraries",
     ],
     install_requires=requires,
 )
--- tests/test_effect_matcher.py
+++ tests/test_effect_matcher.py
@@ -6,51 +6,38 @@ from edl import EffectMatcher, List, Event, Effect
 
 
 class EffectMatcherTestCase(unittest.TestCase):
-    """tests the edl.edl.EffectMatcher class
-    """
+    """tests the edl.edl.EffectMatcher class"""
 
     def setup(self):
-        """set up the tests
-        """
+        """set up the tests"""
         pass
 
     def test_EffectMatcher_regex_is_working_properly(self):
-        """testing if the EffectMatcher.regex is working properly
-        """
-        test_line = 'EFFECTS NAME IS Constant Power'
+        """testing if the EffectMatcher.regex is working properly"""
+        test_line = "EFFECTS NAME IS Constant Power"
         e = EffectMatcher()
 
         m = re.search(e.regex, test_line)
 
         self.assertIsNotNone(m)
 
-        self.assertEqual(
-            'Constant Power',
-            m.group(2).strip()
-        )
+        self.assertEqual("Constant Power", m.group(2).strip())
 
     def test_EffectMatcher_apply_is_working_properly(self):
-        """testing if the EffectMatcher.apply() is working properly
-        """
-        ed_list = List('24')
+        """testing if the EffectMatcher.apply() is working properly"""
+        ed_list = List("24")
         event = Event({})
         event.transition = Effect()
         ed_list.append(event)
 
         e = EffectMatcher()
 
-        test_line = 'EFFECTS NAME IS Constant Power'
+        test_line = "EFFECTS NAME IS Constant Power"
         e.apply(ed_list, test_line)
 
-        self.assertEqual(
-            'Constant Power',
-            event.transition.effect
-        )
+        self.assertEqual("Constant Power", event.transition.effect)
 
-        test_line = 'EFFECTS NAME IS CROSS DISSOLVE'
+        test_line = "EFFECTS NAME IS CROSS DISSOLVE"
         e.apply(ed_list, test_line)
 
-        self.assertEqual(
-            'CROSS DISSOLVE',
-            event.transition.effect
-        )
+        self.assertEqual("CROSS DISSOLVE", event.transition.effect)
